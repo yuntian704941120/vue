a8a9818d7d266ef13ad382ee669f6e8022f128aa
wuyuntian <yuntian704941120@163.com>
2019-08-09 00:05:51 +0800

    001

---
+++ src/components/First.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><h1>{{message}}</h1></div>
+</template>
+
+<script>
+  export default {
+    name: 'First.vue',
+    data () {
+      return {
+        message: 'welcome'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
